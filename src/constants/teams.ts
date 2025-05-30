b8bef9145082b34559e08e21af23c2bd27cb80ab
ravinder9280 <ravinder92809@gmail.com>
2025-05-30 14:48:56 +0530

    2c

--- src/constants/teams.ts
+++ src/constants/teams.ts
@@ -1,125 +1,125 @@
-// data/teams.ts
-import { TeamStats } from '@/types'
+// // data/teams.ts
+// import { TeamStats } from '@/types'
 
-export const teams: TeamStats[] = [
-  {
-    name: 'Mumbai Indians',
-    color: '#004BA0',
-    matches: 247,
-    wins: 138,
-    losses: 105,
-    winPercentage: 55.87,
-    totalRuns: 7255,
-    totalWickets: 826,
-    homeGround: 'Wankhede Stadium',
-    coordinates: {
-      lat: 18.9389,
-      lng: 72.8258
-    }
-  },
-  {
-    name: 'Chennai Super Kings',
-    color: '#FFFF00',
-    matches: 225,
-    wins: 131,
-    losses: 91,
-    winPercentage: 58.22,
-    totalRuns: 7115,
-    totalWickets: 850,
-    homeGround: 'M. A. Chidambaram Stadium',
-    coordinates: {
-      lat: 13.0629,
-      lng: 80.2794
-    }
-  },
-  {
-    name: 'Royal Challengers Bangalore',
-    color: '#EC1C24',
-    matches: 241,
-    wins: 114,
-    losses: 120,
-    winPercentage: 47.3,
-    totalRuns: 7401,
-    totalWickets: 792,
-    homeGround: 'M. Chinnaswamy Stadium',
-    coordinates: {
-      lat: 12.9783,
-      lng: 77.6408
-    }
-  },
-  {
-    name: 'Kolkata Knight Riders',
-    color: '#3A225D',
-    matches: 237,
-    wins: 123,
-    losses: 109,
-    winPercentage: 51.89,
-    totalRuns: 7124,
-    totalWickets: 820,
-    homeGround: 'Eden Gardens',
-    coordinates: {
-      lat: 22.5646,
-      lng: 88.3433
-    }
-  },
-  {
-    name: 'Delhi Capitals',
-    color: '#00008B',
-    matches: 239,
-    wins: 109,
-    losses: 124,
-    winPercentage: 46.13,
-    totalRuns: 6884,
-    totalWickets: 789,
-    homeGround: 'Arun Jaitley Stadium',
-    coordinates: {
-      lat: 28.6379,
-      lng: 77.2431
-    }
-  },
-  {
-    name: 'Rajasthan Royals',
-    color: '#254AA5',
-    matches: 224,
-    wins: 109,
-    losses: 108,
-    winPercentage: 48.66,
-    totalRuns: 6740,
-    totalWickets: 770,
-    homeGround: 'Sawai Mansingh Stadium',
-    coordinates: {
-      lat: 26.9124,
-      lng: 75.7873
-    }
-  },
-  {
-    name: 'Sunrisers Hyderabad',
-    color: '#F26522',
-    matches: 172,
-    wins: 84,
-    losses: 83,
-    winPercentage: 48.83,
-    totalRuns: 5253,
-    totalWickets: 660,
-    homeGround: 'Rajiv Gandhi Stadium',
-    coordinates: {
-      lat: 17.4065,
-      lng: 78.5502
-    }
-  },
-  {
-    name: 'Punjab Kings',
-    color: '#ED1B24',
-    matches: 238,
-    wins: 108,
-    losses: 126,
-    winPercentage: 45.38,
-    totalRuns: 6833,
-    totalWickets: 765,
-    homeGround: 'Punjab Cricket Association Stadium',
-    coordinates: {
-      lat: 30.6909,
-      lng: 76.7373
-    }
-  }
-]
+// export const teams: TeamStats[] = [
+//   {
+//     name: 'Mumbai Indians',
+//     color: '#004BA0',
+//     matches: 247,
+//     wins: 138,
+//     losses: 105,
+//     winPercentage: 55.87,
+//     totalRuns: 7255,
+//     totalWickets: 826,
+//     homeGround: 'Wankhede Stadium',
+//     coordinates: {
+//       lat: 18.9389,
+//       lng: 72.8258
+//     }
+//   },
+//   {
+//     name: 'Chennai Super Kings',
+//     color: '#FFFF00',
+//     matches: 225,
+//     wins: 131,
+//     losses: 91,
+//     winPercentage: 58.22,
+//     totalRuns: 7115,
+//     totalWickets: 850,
+//     homeGround: 'M. A. Chidambaram Stadium',
+//     coordinates: {
+//       lat: 13.0629,
+//       lng: 80.2794
+//     }
+//   },
+//   {
+//     name: 'Royal Challengers Bangalore',
+//     color: '#EC1C24',
+//     matches: 241,
+//     wins: 114,
+//     losses: 120,
+//     winPercentage: 47.3,
+//     totalRuns: 7401,
+//     totalWickets: 792,
+//     homeGround: 'M. Chinnaswamy Stadium',
+//     coordinates: {
+//       lat: 12.9783,
+//       lng: 77.6408
+//     }
+//   },
+//   {
+//     name: 'Kolkata Knight Riders',
+//     color: '#3A225D',
+//     matches: 237,
+//     wins: 123,
+//     losses: 109,
+//     winPercentage: 51.89,
+//     totalRuns: 7124,
+//     totalWickets: 820,
+//     homeGround: 'Eden Gardens',
+//     coordinates: {
+//       lat: 22.5646,
+//       lng: 88.3433
+//     }
+//   },
+//   {
+//     name: 'Delhi Capitals',
+//     color: '#00008B',
+//     matches: 239,
+//     wins: 109,
+//     losses: 124,
+//     winPercentage: 46.13,
+//     totalRuns: 6884,
+//     totalWickets: 789,
+//     homeGround: 'Arun Jaitley Stadium',
+//     coordinates: {
+//       lat: 28.6379,
+//       lng: 77.2431
+//     }
+//   },
+//   {
+//     name: 'Rajasthan Royals',
+//     color: '#254AA5',
+//     matches: 224,
+//     wins: 109,
+//     losses: 108,
+//     winPercentage: 48.66,
+//     totalRuns: 6740,
+//     totalWickets: 770,
+//     homeGround: 'Sawai Mansingh Stadium',
+//     coordinates: {
+//       lat: 26.9124,
+//       lng: 75.7873
+//     }
+//   },
+//   {
+//     name: 'Sunrisers Hyderabad',
+//     color: '#F26522',
+//     matches: 172,
+//     wins: 84,
+//     losses: 83,
+//     winPercentage: 48.83,
+//     totalRuns: 5253,
+//     totalWickets: 660,
+//     homeGround: 'Rajiv Gandhi Stadium',
+//     coordinates: {
+//       lat: 17.4065,
+//       lng: 78.5502
+//     }
+//   },
+//   {
+//     name: 'Punjab Kings',
+//     color: '#ED1B24',
+//     matches: 238,
+//     wins: 108,
+//     losses: 126,
+//     winPercentage: 45.38,
+//     totalRuns: 6833,
+//     totalWickets: 765,
+//     homeGround: 'Punjab Cricket Association Stadium',
+//     coordinates: {
+//       lat: 30.6909,
+//       lng: 76.7373
+//     }
+//   }
+// ]
